825e3ee2dfaf58af9002ec94f260b5137156640a
Caglar Kilcioglu <ckilcioglu@gmail.com>
2020-01-31 16:59:10 +0300

    Fix typo at user auth save db location

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -15,7 +15,7 @@ class UserProfileManager(BaseUserManager):
         user = self.model(email=email, name=name)
 
         user.set_password(password)
-        user.save(using=slef._db)
+        user.save(using=self._db)
 
         return user
 
