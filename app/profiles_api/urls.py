d1246bea24b7343492af76e16d21d1a3a390ea73
Caglar Kilcioglu <ckilcioglu@gmail.com>
2021-09-14 19:27:01 +0300

    Added login API

--- app/profiles_api/urls.py
+++ app/profiles_api/urls.py
@@ -11,5 +11,6 @@ router.register('phone-book', views.PhoneBookViewSet, basename='phone-book')
 
 urlpatterns= [
     path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
     path('', include(router.urls))
 ]
--- app/profiles_api/views.py
+++ app/profiles_api/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 from django.shortcuts import get_object_or_404
 
 from profiles_api import serializers
@@ -110,6 +112,12 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email')
+
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handles creating user authentication tokens"""
+    # To enable in browsable API view
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
 
 
 class PhoneBookViewSet(viewsets.ViewSet):
